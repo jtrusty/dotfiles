c90aa93ec787d8274961e4c87780c4f523f136d1
Justin Trusty <justintrusty@selfsvc.net>
2025-06-13 12:07:23 -0500

    updated battery symbols

--- .user.zsh
+++ .user.zsh
@@ -41,6 +41,9 @@ alias fastfetch='fastfetch --logo-type kitty'
 alias update-all='sudo pacman -Syu --noconfirm archlinux-keyring;yay -Syu --noconfirm; flatpak update -y; mise self-update;mise upgrade'
 alias cat='bat --theme=ansi'
 alias htop='btop'
+alias look="find . -name"
+alias search="grep --color -rnw ./ -e "
+alias ports="lsof -PiTCP -sTCP:LISTEN"
 
 # # IP Addresses
 alias publicip='echo "IPv4: $(curl -4 -s icanhazip.com)"; echo "IPv6: $(curl -6 -s icanhazip.com)"'
@@ -96,3 +99,72 @@ plugins=(
 
 # # Dotfiles
 alias config='/usr/bin/git --git-dir=/home/justin/.dotfiles/ --work-tree=/home/justin'
+
+# # Git Functions for Logs
+LOG_HASH="%C(always,yellow)%h%C(always,reset)"
+LOG_RELATIVE_TIME="%C(always,green)(%ar)%C(always,reset)"
+LOG_AUTHOR="%C(always,blue)<%an>%C(always,reset)"
+LOG_REFS="%C(always,red)%d%C(always,reset)"
+LOG_SUBJECT="%s"
+
+LOG_FORMAT="$LOG_HASH}$LOG_AUTHOR}$LOG_RELATIVE_TIME}$LOG_SUBJECT $LOG_REFS"
+
+BRANCH_PREFIX="%(HEAD)"
+BRANCH_REF="%(color:red)%(color:bold)%(refname:short)%(color:reset)"
+BRANCH_HASH="%(color:yellow)%(objectname:short)%(color:reset)"
+BRANCH_DATE="%(color:green)(%(committerdate:relative))%(color:reset)"
+BRANCH_AUTHOR="%(color:blue)%(color:bold)<%(authorname)>%(color:reset)"
+BRANCH_CONTENTS="%(contents:subject)"
+
+BRANCH_FORMAT="}$BRANCH_PREFIX}$BRANCH_REF}$BRANCH_HASH}$BRANCH_DATE}$BRANCH_AUTHOR}$BRANCH_CONTENTS"
+
+show_git_head() {
+    pretty_git_log -1
+    git show -p --pretty="tformat:"
+}
+
+pretty_git_log() {
+    git log --since="6 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
+}
+
+pretty_git_log_all() {
+    git log --all --since="6 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
+}
+
+
+pretty_git_branch() {
+    git branch -v --color=always --format=${BRANCH_FORMAT} $* | pretty_git_format
+}
+
+pretty_git_branch_sorted() {
+    git branch -v --color=always --format=${BRANCH_FORMAT} --sort=-committerdate $* | pretty_git_format
+}
+
+pretty_git_format() {
+    # Replace (2 years ago) with (2 years)
+    sed -Ee 's/(^[^)]*) ago\)/\1)/' |
+    # Replace (2 years, 5 months) with (2 years)
+    sed -Ee 's/(^[^)]*), [[:digit:]]+ .*months?\)/\1)/' |
+    # Shorten time
+    sed -Ee 's/ seconds?\)/s\)/' |
+    sed -Ee 's/ minutes?\)/m\)/' |
+    sed -Ee 's/ hours?\)/h\)/' |
+    sed -Ee 's/ days?\)/d\)/' |
+    sed -Ee 's/ weeks?\)/w\)/' |
+    sed -Ee 's/ months?\)/M\)/' |
+    # Shorten names
+    sed -Ee 's/<Andrew Burgess>/<me>/' |
+    sed -Ee 's/<([^ >]+) [^>]*>/<\1>/' |
+    # Line columns up based on } delimiter
+    column -s '}' -t
+}
+
+git_page_maybe() {
+    # Page only if we're asked to.
+    if [ -n "${GIT_NO_PAGER}" ]; then
+        cat
+    else
+        # Page only if needed.
+        less --quit-if-one-screen --no-init --RAW-CONTROL-CHARS --chop-long-lines
+    fi
+}
